bcf991ee10d75070fc08d81c34dac03dfa192175
MuhammedNadeer <muhammednadeerpc@gmail.com>
2024-04-06 23:29:13 +0530

    add forms page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -204,6 +204,44 @@ export default function CandidateInstructionsCardBased() {
               <h1 className="text-3xl font-bold text-gray-50 tracking-tighter sm:text-5xl">Problem Statements</h1>
               <p className="text-gray-100 ml-1 dark:text-gray-400">Problem Statements are given below</p>
             </div>
+            <div className="space-y-4">
+              <Card style={{ backgroundColor: '#1B1B1B', color: 'white', borderColor: 'rgba(0, 0, 0, 0.2)' }}>
+                <CardHeader>
+                  <div>Problem Statement 1</div>
+                </CardHeader>
+                <CardContent>
+                  <ol className="list-disc ml-4 space-y-2">
+                    <li>The problem statements and their descriptions will be accessible under the "Problem Statements" tab.</li>
+                    <li>At 10:00 AM, the form for idea submission will be closed for all participants. Late submissions will not be considered,so make sure you submit your ideas on time.</li>
+                    <li>The form will be available at the "Form Submission" tab.</li>
+                  </ol>
+                </CardContent>
+              </Card>
+              <Card style={{ backgroundColor: '#1B1B1B', color: 'white', borderColor: 'rgba(0, 0, 0, 0.2)' }}>
+                <CardHeader>
+                  <div>Problem Statement 2</div>
+                </CardHeader>
+                <CardContent>
+                  <ol className="list-disc ml-4 space-y-2">
+                    <li>All presentation documents must be completed and uploaded by 2:30 PM. This includes  pitch deck, hosted vercel link and GitHub repository link.You can find the form located at the bottom of this page.</li>
+                    <li>You can find the form located at "Form Submission" tab.</li>
+                    <li>Ensure that your team has all necessary documents ready and uploaded before the deadline.</li>
+                  </ol>
+                </CardContent>
+              </Card>
+              <Card style={{ backgroundColor: '#1B1B1B', color: 'white', borderColor: 'rgba(0, 0, 0, 0.2)' }}>
+                <CardHeader>
+                  <div>Open Track</div>
+                </CardHeader>
+                <CardContent>
+                  <ol className="list-disc ml-4 space-y-2">
+                    <li>All presentation documents must be completed and uploaded by 2:30 PM. This includes  pitch deck, hosted vercel link and GitHub repository link.You can find the form located at the bottom of this page.</li>
+                    <li>You can find the form located at "Form Submission" tab.</li>
+                    <li>Ensure that your team has all necessary documents ready and uploaded before the deadline.</li>
+                  </ol>
+                </CardContent>
+              </Card>
+            </div>
           </div>
           </main>
           <div className="flex mx-auto max-w-3xl mt-6">
@@ -226,7 +264,7 @@ export default function CandidateInstructionsCardBased() {
           </div></>
         }
 
-        {activeMenu==="final"&&<><main
+        {(activeMenu==="final"&& rule === "innov8-coet-24") && <><main
           className="flex-1 grid min-h-[600px] gap-6 px-4 text-base lg:gap-10 lg:px-6 xl:min-h-[800px]"
           style={{ marginTop: '70px' }}
         >
@@ -235,9 +273,34 @@ export default function CandidateInstructionsCardBased() {
               <h1 className="text-3xl font-bold text-gray-50 tracking-tighter sm:text-5xl">Form Submissions</h1>
               <p className="text-gray-100 ml-1 dark:text-gray-400">All your forms are shown below</p>
             </div>
+            <div className="space-y-2">
+            <Card style={{ backgroundColor: '#1B1B1B', color: 'white', borderColor: 'rgba(0, 0, 0, 0.2)' }}>
+                <CardHeader>
+                  <div>Idea Submission Form:</div>
+                </CardHeader>
+                <CardContent>
+                  <ol className="ml-4 space-y-2">
+                    <li>Click the button to fill Idea Submission Form</li>
+                  </ol>
+                  <a href="https://forms.gle/txEHzxvPDH9QvLRw5" target="_blank"><button className="ml-4 mt-2 bg-transparent hover:bg-gray-800 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Go to form</button></a>
+                </CardContent>
+              </Card>
+            </div>
+            <div className="space-y-2">
+            <Card style={{ backgroundColor: '#1B1B1B', color: 'white', borderColor: 'rgba(0, 0, 0, 0.2)' }}>
+                <CardHeader>
+                  <div>Final Submission Form:</div>
+                </CardHeader>
+                <CardContent>
+                  <ol className="ml-4 space-y-2">
+                    <li>Click the button to fill Idea Submission Form</li>
+                  </ol>
+                  <a href="https://forms.gle/gPEfYaCaQthbyjUF8" target="_blank"><button className="ml-4 mt-2 bg-transparent hover:bg-gray-800 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Go to form</button></a>
+                </CardContent>
+              </Card>
+            </div>
           </div>
-        </main>
-        <div className="mx-auto max-w-3xl mt-6">
+          <div className="mx-auto max-w-3xl mt-6">
             <div style={{ marginBottom: '100px' }}>
               <Button
                 className='w-full bg-black text-white hover:bg-gray-900'
@@ -246,8 +309,57 @@ export default function CandidateInstructionsCardBased() {
                 Previous
               </Button>
             </div>
-          </div></>}
-
+          </div>
+        </main>
+        </>}
+        {(activeMenu==="final"&& rule === "innov8-coet-6") && <><main
+          className="flex-1 grid min-h-[600px] gap-6 px-4 text-base lg:gap-10 lg:px-6 xl:min-h-[800px]"
+          style={{ marginTop: '70px' }}
+        >
+          <div className="mx-auto max-w-3xl space-y-6">
+            <div className="space-y-2">
+              <h1 className="text-3xl font-bold text-gray-50 tracking-tighter sm:text-5xl">Form Submissions</h1>
+              <p className="text-gray-100 ml-1 dark:text-gray-400">All your forms are shown below</p>
+            </div>
+            <div className="space-y-2">
+            <Card style={{ backgroundColor: '#1B1B1B', color: 'white', borderColor: 'rgba(0, 0, 0, 0.2)' }}>
+                <CardHeader>
+                  <div>Idea Submission Form:</div>
+                </CardHeader>
+                <CardContent>
+                  <ol className="ml-4 space-y-2">
+                    <li>Click the button to fill Idea Submission Form</li>
+                  </ol>
+                  <a href="https://forms.gle/9oNQpJiBSyXY6o5Z8" target="_blank"><button className="ml-4 mt-2 bg-transparent hover:bg-gray-800 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Go to form</button></a>
+                </CardContent>
+              </Card>
+            </div>
+            <div className="space-y-2">
+            <Card style={{ backgroundColor: '#1B1B1B', color: 'white', borderColor: 'rgba(0, 0, 0, 0.2)' }}>
+                <CardHeader>
+                  <div>Final Submission Form:</div>
+                </CardHeader>
+                <CardContent>
+                  <ol className="ml-4 space-y-2">
+                    <li>Click the button to fill Idea Submission Form</li>
+                  </ol>
+                  <a href="https://forms.gle/5H28YskkLdAYkirh6" target="_blank"><button className="ml-4 mt-2 bg-transparent hover:bg-gray-800 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Go to form</button></a>
+                </CardContent>
+              </Card>
+            </div>
+          </div>
+          <div className="mx-auto max-w-3xl mt-6">
+            <div style={{ marginBottom: '100px' }}>
+              <Button
+                className='w-full bg-black text-white hover:bg-gray-900'
+                onClick={() => handleMenuClick("problem")}
+              >
+                Previous
+              </Button>
+            </div>
+          </div>
+        </main>
+        </>}
         
       </div></>
   );
